421728e9793657ce96afbd87dbf10327125cfbb3
Arnaud Gonin <arnaud.gonin@live.fr>
2018-03-30 11:19:00 +0200

    initialisation duprojet

---
+++ cmake-build-debug/CMakeFiles/Projet2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet2.dir/main.cpp.obj"
+  "Projet2.pdb"
+  "Projet2.exe"
+  "Projet2.exe.manifest"
+  "libProjet2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projet2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
